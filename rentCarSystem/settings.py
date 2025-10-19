48ec7d586d30cf476f56cb70b02e63b3343f7468
HamzaAlkhateeb97 <134374970+HamzaAlkhateeb97@users.noreply.github.com>
2025-10-19 14:00:25 +0300

    Update settings and add new config file

--- rentCarSystem/settings.py
+++ rentCarSystem/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from datetime import timedelta
 from pathlib import Path
+from apps.my_apps import MY_APPS
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -42,7 +43,10 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'django_filters',
+
+
 ]
+INSTALLED_APPS += MY_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -79,8 +83,12 @@ WSGI_APPLICATION = 'rentCarSystem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+         'ENGINE': 'django.db.backends.postgresql',
+         'NAME': 'rent_car_db',
+         'USER': 'user',
+         'PASSWORD': 'pass',
+         'HOST': 'localhost',
+         'PORT': '5432',
     }
 }
 
