b4b6994dce1b214fa168a26adb9e7907e450132f
HamzaAlkhateeb97 <134374970+HamzaAlkhateeb97@users.noreply.github.com>
2025-10-20 21:06:21 +0300

    add api's for vehicle

--- apps/vehicle/models.py
+++ apps/vehicle/models.py
@@ -16,7 +16,7 @@ class Vehicle(models.Model):
     plate_number = models.CharField(max_length=20,unique=True)
     description = models.TextField(blank=True, null=True)
     image = models.ImageField(upload_to='vehicles/', blank=True, null=True)
-    id_available = models.BooleanField(default=True)
+    is_available = models.BooleanField(default=True)
 
     def __str__(self):
-        return f"{self.brand} {self.model} ({self.plate_number})"
+        return f"{self.brand} {self.year} ({self.plate_number})"
