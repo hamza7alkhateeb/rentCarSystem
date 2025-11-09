8ca3fcb21de0ddd6378f7a4a4f6455863da683df
HamzaAlkhateeb97 <134374970+HamzaAlkhateeb97@users.noreply.github.com>
2025-11-09 11:24:06 +0300

    add refresh api and fix logic permissions

--- apps/authentication/urls.py
+++ apps/authentication/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import AuthViewSet
-
+from rest_framework_simplejwt.views import TokenRefreshView
 auth_list = AuthViewSet.as_view({
     'post': 'register'
 })
@@ -11,4 +11,5 @@ auth_login = AuthViewSet.as_view({
 urlpatterns = [
     path('register/', auth_list, name='register'),
     path('login/', auth_login, name='login'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
