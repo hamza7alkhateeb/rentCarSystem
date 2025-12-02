226b44ac3e0eecae6310ba6fd456d1d3e43e7bff
Hamza Alkhateeb <hamza.alkhateeb@sitech.me>
2025-12-02 10:50:45 +0300

    add task and setting for admin dashboard

--- apps/customer/tasks.py
+++ apps/customer/tasks.py
@@ -3,9 +3,8 @@ from .models import Customer
 from .external_api import get_customer_status_mock
 
 
-@shared_task(name="apps.customer.tasks.sync_customer_status_task")
-def sync_customer_status_task():
-    print('test')
+@shared_task
+def sync_customers_status_task():
     customers = Customer.objects.all()
     updated = 0
     for customer in customers:
@@ -15,3 +14,14 @@ def sync_customer_status_task():
         updated += 1
 
     return f"{updated} customers status updated successfully"
+
+@shared_task
+def sync_single_customer_status_task(customer_id):
+
+    customer = Customer.objects.get(id=customer_id)
+
+    new_status = get_customer_status_mock(customer.id)
+    customer.status = new_status
+    customer.save()
+
+    return f"Customer {customer_id} status updated successfully"
